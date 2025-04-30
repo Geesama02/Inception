77ec147d8eca1bf27295ad99c62df3b2d5a0c951
Geesama02 <oussama.thecreative@gmail.com>
2025-04-30 16:31:57 +0100

    Redis Added

--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -e
+set -e
 set -x
 
 until mysqladmin ping -h"${WORDPRESS_DB_HOST}" --silent; do
@@ -12,24 +12,35 @@ echo "WORDPRESS_DB_NAME $WORDPRESS_DB_NAME;
       WORDPRESS_DB_USER $WORDPRESS_DB_USER;
       WORDPRESS_USER_PASSWORD $WORDPRESS_USER_PASSWORD;
       "
+if [ ! -f /var/www/html/wp-config.php ]; then
 
-wp config create --dbname="${WORDPRESS_DB_NAME}" \
-                --dbhost="${WORDPRESS_DB_HOST}" \
-                --dbuser="${WORDPRESS_DB_USER}" \
-                --dbpass="${WORDPRESS_USER_PASSWORD}" \
-                --path="/var/www/html" \
-                --allow-root
+    wp config create --dbname="${WORDPRESS_DB_NAME}" \
+                    --dbhost="${WORDPRESS_DB_HOST}" \
+                    --dbuser="${WORDPRESS_DB_USER}" \
+                    --dbpass="${WORDPRESS_USER_PASSWORD}" \
+                    --path="/var/www/html" \
+                    --allow-root
+fi
 
-wp core install --url="${WORDPRESS_URL}" \
-                --title="${WORDPRESS_TITLE}" \
-                --admin_user="${WORDPRESS_ADMIN_USER}" \
-                --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-                --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
-                --path="/var/www/html" \
-                --skip-email \
-                --allow-root
 
-wp option update siteurl "https://localhost:8080" --allow-root
-wp option update home "https://localhost:8080" --allow-root
+if ! wp core is-installed --allow-root; then
+    wp core install --url="${WORDPRESS_URL}" \
+                    --title="${WORDPRESS_TITLE}" \
+                    --admin_user="${WORDPRESS_ADMIN_USER}" \
+                    --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+                    --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+                    --path="/var/www/html" \
+                    --skip-email \
+                    --allow-root
+fi
+
+wp option update siteurl "$WORDPRESS_URL" --allow-root
+wp option update home "$WORDPRESS_URL" --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp config set WP_CACHE true --allow-root
+wp redis enable --allow-root
 
 exec php-fpm7.4 -F
